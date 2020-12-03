199e4d8fdff1073d6b7be3aec8b9080590cff036
JeffersonCharlles <contato@jefferdeveloper.com>
2020-12-03 00:07:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+
+
+
+# Auto Install Lamp ubuntu server 20.04
+
+
+
+a ideia aqui e criar um script de lâmpada de instalação automática onde ele fara todas as configurações padrao para funcionar sozinho e gera o root e password para seu mysql
+
+## About this Project
+
+## Functionalities
+
+- auto install apache
+
+- auto install update
+
+- auto install upgrade
+
+- auto install wget unzip git
+
+- auto install php7.4 php7.4-cli php7.4-curl php7.4-gd php7.4-json php7.4-xml php7.4-zip php7.4-mbstring php7.4-mysql
+
+- auto install install mariadb-server
+	- funcoes ainda nao testado configuracoes do mariadb
+	
+- o script tambem gera um relatorio de algumas informacoes da maquina
+
+  
+
+  
+
+
+## Getting Started
+
+### Prerequisites
+
+ubuntu 20.04
+
+### Installing
+
+**Cloning the Repository** **Installing dependencies**
+
+```
+$ git clone https://github.com/Jeffersoncharlles/auto_install_Lamp.git
+
+$ cd auto_install_Lamp 
+$ sudo chmod +x auto_install_Lamp-20-04.sh
+$ sudo bash ./auto_install_Lamp-20-04.sh
+```
+
+
+
+## Contributing
+
+You can send how many PR's do you want, I'll be glad to analyse and accept them! And if you have any question about the project...
+
+Email-me: contato@jefferdeveloper.com
+
+Connect with me at [LinkedIn](https://www.linkedin.com/in/jefferdeveloper/)
+
+Thank you!
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/steniowagner/mindCast/blob/master/LICENSE) file for details
---
+++ auto_install_Lamp-20-04.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+##########################################################################
+##########################################################################
+#=========+++++++++SCRIPT INSTALL APACHE MYSQL PHP+++++++++++=============
+#
+#Autor: Jefferson Charlles
+#Data Criacao:23/11/2020
+#
+#Descricao: Script auto instalador php apache e mariadb
+#
+#====Versao 1.1
+###########################################################################
+# Histórico:
+#
+#   v1.0 22/11/2020, Jefferson:
+#       - Início do programa
+#
+#   v1.1 23/11/2020, Jefferson:
+#       - comecando a organizar o programa#   
+#       
+#   v1.2 02/12/2020, Jefferson:
+#       - password md5 altogerador#  		
+# ------------------------------------------------------------------------#
+# ------------------------------- VARIÁVEIS ------------------------------#
+LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
+KERNEL=$(uname -r)
+HOSTNAME=$(hostname)
+CPUNO=$(cat /proc/cpuinfo |grep "model name" | wc -l) #numero de processador
+CPUMODEL=$(cat /proc/cpuinfo |grep "model name" | head -n1|cut -c14-) #modelo do processador
+MEMTOTAL=$(expr $(cat /proc/meminfo |grep MemTotal|tr -d ' '|cut -d: -f2|tr -d kB) / 1024) # em mb
+UPTIME=$(uptime -s)
+
+PASSWORD=$(date +%s | sha256sum | base64 | head -c 32) 
+
+#CORES
+AMARELO="\033[33;1m"
+CINZACLARRO="\033[37;1m"
+VERDE="\033[32;1m"
+VERMELHO="\033[31;1m"
+ZERARR="\033[0m"
+
+# ------------------------------------------------------------------------#
+echo -e "==========================================================="
+echo -e "Relatorio da Maquina: $HOSTNAME"
+echo -e "Data/Hora: $(date)"
+echo -e "==========================================================="
+echo -e
+echo -e "Maquina Ativa desde: $UPTIME"
+echo -e
+echo -e "Versao do Kernel: $KERNEL"
+echo -e
+echo -e "CPUs:"
+echo -e "Quantidade de Cpus/Core: $CPUNO"
+echo -e "Modelo da Cpu: ${AMARELO}$CPUMODEL${ZERARR}"
+echo -e
+echo -e "Memoria Total: ${AMARELO}$MEMTOTAL MB${ZERARR}"
+echo "####################################################################################"
+echo "#########################----------update----#######################################"
+sudo apt-get update >>$LOG
+echo "#########################----------upgrade----#######################################"
+sudo apt-get full-upgrade -y >>$LOG
+echo "#########################----------unzip--git----####################################"
+sudo apt-get install wget unzip git -y >>$LOG
+echo "#########################----------apache2----#######################################"
+sudo apt-get install apache2 -y >>$LOG
+echo "#########################----------php7.4----#######################################"
+sudo apt-get install php7.4 php7.4-cli php7.4-curl php7.4-gd php7.4-json php7.4-xml php7.4-zip php7.4-mbstring php7.4-mysql -y >>$LOG
+echo "#########################----------mariadb----#######################################"
+sudo apt-get install mariadb-server -y >>$LOG
+echo "####################################################################################"
+echo "####################################################################################"
+echo "$PASSWORD"
+echo -e
+mysql_secure_installation
+
+#remover usuario anonimo
+echo "##################################-remover-usuario-anonimo-############################" 
+y
+#desabilitar login remoto no root
+echo "##############################-desabilitar-login-remoto-no-root-######################"  
+y
+#remover banco de dados teste e acesso a ele
+echo "############################-remover-banco-de-dados-teste-############################"  
+y
+#recaregar os privilegios de tabelas 
+y
+
+mysql -uroot
+
+#CREATE USER 'user'@'localhost' INDENTIFIED BY 'senhausuario';
+#GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'superadmin'@'localhost' IDENTIFIED BY '$PASSWORD';
+exit
+echo "#########################----------Restart-aplicacao----###################################"
+a2enmod suexec rewrite include
+systemctl restart apache2
+
+#sudo apt install phpmyadmin
